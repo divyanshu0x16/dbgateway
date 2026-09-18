bec4225bb3c75e258f07784252a0317415ea2711
divyanshu0x16 <divyanshu22.dm@gmail.com>
2026-09-18 15:18:54 +0530

    added redisclient

---
+++ redisclient/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func main() {
+	client := redis.NewClient(&redis.Options{
+		Addr: "127.0.0.1:6379",
+	})
+
+	ctx := context.Background()
+
+	err := client.Set(ctx, "foo", "bar", 0).Err()
+
+	if err != nil {
+		panic(err)
+	}
+
+	val, err := client.Get(ctx, "foo").Result()
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("foo =", val)
+}
