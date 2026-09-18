8ab6abfe2359ff536c44aaaa2ea2b1285808171c
divyanshu0x16 <divyanshu22.dm@gmail.com>
2026-09-18 12:40:14 +0530

    added basic TCP connection support

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
 import "fmt"
+import "net"
 
 func main() {
 	fmt.Println("hello from dbgateway")
+	listener, err := net.Listen("tcp", ":7000")
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("listening on :7000")
+
+	for{
+		conn, err := listener.Accept()
+
+		if err != nil {
+			fmt.Println("accept error:", err)
+			continue
+		}
+
+		fmt.Println("client connected:", conn.RemoteAddr())
+	}
 }
